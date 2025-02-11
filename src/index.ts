fcd7ec3c952e6ba49a3546576b621046055f2b9f
qyj <qyjbeijing@gmail.com>
2025-02-11 17:54:42 +0800

    ADD: 简化类型声明，添加测试

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { MotorFloat } from "./types/float.type";
-import { MotorReference } from "./reference";
-
-const a = new MotorReference(MotorFloat, 0.5)
-const b = new MotorReference(MotorFloat, a)
-console.log(b.rawValue) // 0.5
--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,7 @@
 import { MotorInstance } from "./instance";
 import { MotorMemory } from "./memory";
 
-export interface MotorType<RawType, MotorInstanceType extends MotorInstance<RawType> = MotorInstance<RawType>> {
+export interface MotorType<RawType> {
     readonly size: number;
-    new(defaultVal?: any, memory?: MotorMemory, address?: number): MotorInstanceType;
+    new(defaultVal?: any, memory?: MotorMemory, address?: number): MotorInstance<RawType>;
 }
