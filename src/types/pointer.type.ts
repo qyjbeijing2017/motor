76b6413c59a6d3ff13ec2e25fc6b8e8b681c8f6a
qyj <qyjbeijing@gmail.com>
2025-02-11 15:27:36 +0800

    MOD: 重置String为引用类型CharArray

--- src/types/pointer.type.ts
+++ src/types/pointer.type.ts
@@ -3,10 +3,7 @@ import { MotorMemory } from "../memory";
 import { MotorType } from "../type";
 import { MotorTypeOf } from "../utils/type-of";
 
-
 export abstract class MotorPointer<T extends MotorInstance<any>> extends MotorInstance<number> {
-    static size: number = 4;
-
     protected read(): number {
         return this.memory.dataView.getUint32(this.address, true);
     }
@@ -24,7 +21,7 @@ export abstract class MotorPointer<T extends MotorInstance<any>> extends MotorIn
 
 export function motorDefinePointer<T extends MotorType<any>>(type: T): MotorTypeOf<MotorPointer<InstanceType<T>>> {
     return class extends MotorPointer<InstanceType<T>> {
-        static size: number = 4;
+        static readonly size: number = 4;
         get raw(): InstanceType<T> {
             return new type(undefined, this.memory, this.rawValue) as InstanceType<T>;
         }
--- src/types/string.type.ts
+++ src/types/string.type.ts
@@ -1,37 +1,62 @@
 import { MotorInstance } from "../instance";
 import { MotorMemory } from "../memory";
+import { MotorArray, motorNewArray } from "./array.type";
+import { MotorChar } from "./char.type";
+import { motorGetPointer, MotorPointer } from "./pointer.type";
 
 export class MotorString extends MotorInstance<string> {
-    static size: number = 1;
-    static decoder = new TextDecoder();
-    static encoder = new TextEncoder();
-    private _length: number = 1;
+    static readonly size = 4;
+    static readonly decoder = new TextDecoder();
+    static readonly encoder = new TextEncoder();
+    private _length: number = 0;
     get length(): number {
         return this._length;
     }
+
     protected read(): string {
-        const stringBuffer = this.memory.buffer.subarray(this.address, this.address + this.length)
-        return MotorString.decoder.decode(stringBuffer);
-    }
-    protected write(value: string): void {
-        const stringBuffer = MotorString.encoder.encode(value);
-        const blockFree = {
-            start: this.address,
-            length: this.length
-        }
-        this._length = stringBuffer.length;
-        this.memory.free(blockFree);
-        this.address = this.memory.allocate(this.length);
-        this.memory.buffer.set(stringBuffer, this.address);
-    }
-    free(): void {
-        this.memory.free({ start: this.address, length: this.length });
+        const bytes = this.memory.buffer.slice(this.charAddress, this.charAddress + this._length);
+        return MotorString.decoder.decode(bytes);
     }
 
-    constructor(defaultValue?: string, memory?: MotorMemory, address?: number) {
-        super(undefined, memory, address);
-        if (defaultValue) {
-            this.rawValue = defaultValue;
+    protected write(value: string): void {
+        this.freeCharArray();
+        this._length = value.length;
+        const encoded = MotorString.encoder.encode(value);
+        const address = this.memory.allocate(encoded.length);
+        this.charAddress = address;
+        const bytes = MotorString.encoder.encode(value)
+        this.memory.buffer.set(bytes, address);
+    }
+
+    protected get charAddress(): number {
+        return this.memory.dataView.getUint32(this.address, true);
+    }
+
+    protected set charAddress(value: number) {
+        this.memory.dataView.setUint32(this.address, value, true);
+    }
+
+    protected freeCharArray(){
+        if(this.charAddress === 0) return;
+        this.memory.free({
+            start: this.charAddress,
+            length: this._length
+        });
+    }
+
+    toCharArray(): MotorArray<MotorChar> {
+        return motorNewArray(MotorChar, this._length, undefined, this.memory, this.charAddress);
+    }
+
+    free(): void {
+        this.freeCharArray();
+        super.free();
+    }
+
+    constructor(defaultValue?: string | MotorInstance<string>, memory?: MotorMemory, address?: number) {
+        super(defaultValue, memory, address);
+        if(defaultValue) {
+            this._length = defaultValue instanceof MotorInstance ? defaultValue.rawValue.length : defaultValue.length;
         }
     }
 }
