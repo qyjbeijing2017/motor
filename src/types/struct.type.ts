02fd3ab24bf488283e3dc0f9994593825e617ac6
qyj <qyjbeijing@gmail.com>
2025-02-12 17:39:42 +0800

    MOD: struct改回来

--- src/types/struct.type.ts
+++ src/types/struct.type.ts
@@ -4,39 +4,9 @@ import { MotorType } from "../type";
 import { MotorRawOf } from "../utils/raw-of";
 
 export abstract class MotorStruct<T extends {[key: string]: MotorType<any>} > extends MotorInstance<{ [K in keyof T]: MotorRawOf<InstanceType<T[K]>> }>{
-    abstract definition: T;
 
     constructor(defaultVal?: { [K in keyof T]: MotorRawOf<InstanceType<T[K]>>; }, memory?: MotorMemory, address?: number) {
         super(defaultVal, memory, address);
-    }
-
-    get<K extends keyof T>(key: K): InstanceType<T[K]> {
-        let offset = 0;
-        for (const k in this.definition) {
-            if (k === key as any) {
-                return new this.definition[k](undefined, this.memory, this.address + offset) as any;
-            }
-            offset += this.definition[k].size;
-        }
-        throw new Error("Invalid key");
-    }
-
-    protected read(): { [K in keyof T]: MotorRawOf<InstanceType<T[K]>>; } {
-        let offset = 0;
-        let result = {} as any;
-        for (const key in this.definition) {
-            result[key] = new this.definition[key](undefined, this.memory, this.address + offset).rawValue;
-            offset += this.definition[key].size;
-        }
-        return result;
-    }
-
-    protected write(value: { [K in keyof T]: MotorRawOf<InstanceType<T[K]>>; }): void {
-        let offset = 0;
-        for (const key in this.definition) {
-            new this.definition[key](value[key], this.memory, this.address + offset);
-            offset += this.definition[key].size;
-        }
     }
 
     static define<T extends {[key: string]: MotorType<any>}>( definition: T){
@@ -45,8 +15,36 @@ export abstract class MotorStruct<T extends {[key: string]: MotorType<any>} > ex
             size += definition[key].size;
         }
         return class extends MotorStruct<T> {
-            definition: T = definition;
             static readonly size = size;
+
+            get<K extends keyof T>(key: K): InstanceType<T[K]> {
+                let offset = 0;
+                for (const k in definition) {
+                    if (k === key as any) {
+                        return new definition[k](undefined, this.memory, this.address + offset) as any;
+                    }
+                    offset += definition[k].size;
+                }
+                throw new Error("Invalid key");
+            }
+        
+            protected read(): { [K in keyof T]: MotorRawOf<InstanceType<T[K]>>; } {
+                let offset = 0;
+                let result = {} as any;
+                for (const key in definition) {
+                    result[key] = new definition[key](undefined, this.memory, this.address + offset).rawValue;
+                    offset += definition[key].size;
+                }
+                return result;
+            }
+        
+            protected write(value: { [K in keyof T]: MotorRawOf<InstanceType<T[K]>>; }): void {
+                let offset = 0;
+                for (const key in definition) {
+                    new definition[key](value[key], this.memory, this.address + offset);
+                    offset += definition[key].size;
+                }
+            }
         }
     }
 }
