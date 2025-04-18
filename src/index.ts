43c792bb754cf5586c452047bb8aadcb977a23d7
qyj <qyjbeijing@163.com>
2025-04-18 16:33:54 +0800

    MOD: instance&type recreate

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,3 @@ export { motorSetFloat8, motorGetFloat8 } from './utils/float8';
 export { MotorOperator } from './il/operator';
 export { MotorILType } from './il/type';
 
-export { MotorInstance } from './instance';
-export { MotorType } from './type';
-
-export { MotorU8, MotorU16, MotorU32, MotorU64 } from './types/unsigned';
-export { MotorI8, MotorI16, MotorI32, MotorI64 } from './types/int';
-export { MotorF8, MotorF16, MotorF32, MotorF64 } from './types/float';
-export { MotorNull } from './types/null';
-export { MotorPointer } from './types/pointer';
-export { MotorArray } from './types/array';
-export { MotorStruct } from './types/struct';
