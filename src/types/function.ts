b2eed4bde2ce897e2160b50058b3b4e8b12974ae
qyj <qyjbeijing@163.com>
2025-04-18 22:06:25 +0800

    ADD: function

---
+++ src/types/function.ts
@@ -0,0 +1,11 @@
+import { MotorInstance } from "../instance";
+
+export class MotorFunction extends MotorInstance<number> {
+    static readonly size = 8;
+    set js(value: number) {
+        this.memory.viewer.setBigUint64(this.address, BigInt(value), true);
+    }
+    get js(): number {
+        return Number(this.memory.viewer.getBigUint64(this.address, true));
+    }
+}
