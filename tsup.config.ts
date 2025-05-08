b8e35dd4c38c5769964afbd6237dced2f29db9a8
qyj <qyjbeijing@163.com>
2025-05-08 15:04:23 +0800

    ADD:git infor，ignore source files

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,26 +9,26 @@ export default defineConfig({
     sourcemap: true,
     clean: true,
     async onSuccess() {
-        const copy = async (target: string, dest: string) => {
-            const stat = await lstat(target)
-            if (stat.isDirectory()) {
-                if (!existsSync(dest)) {
-                    await mkdir(dest, { recursive: true })
-                }
-                const files = await readdir(target)
-                for (const file of files) {
-                    const srcPath = `${target}/${file}`
-                    const destPath = `${dest}/${file}`
-                    await copy(srcPath, destPath)
-                }
-            } else {
-                await copyFile(target, dest)
-            }
-        }
+        // const copy = async (target: string, dest: string) => {
+        //     const stat = await lstat(target)
+        //     if (stat.isDirectory()) {
+        //         if (!existsSync(dest)) {
+        //             await mkdir(dest, { recursive: true })
+        //         }
+        //         const files = await readdir(target)
+        //         for (const file of files) {
+        //             const srcPath = `${target}/${file}`
+        //             const destPath = `${dest}/${file}`
+        //             await copy(srcPath, destPath)
+        //         }
+        //     } else {
+        //         await copyFile(target, dest)
+        //     }
+        // }
 
-        await copy('./package.json', './dist/package.json')
-        await copy('./README.md', './dist/README.md')
-        await copy('./LICENSE', './dist/LICENSE')
-        await copy('./doc', './dist/doc')
+        // await copy('./package.json', './dist/package.json')
+        // await copy('./README.md', './dist/README.md')
+        // await copy('./LICENSE', './dist/LICENSE')
+        // await copy('./doc', './dist/doc')
     },
 })
