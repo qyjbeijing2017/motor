99aa67148d26b3b6ff88def5d49d6a49028e4ebf
qyj <qyjbeijing@163.com>
2025-05-13 15:37:10 +0800

    MOD: create .d

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,9 @@ import { lstat, mkdir, readdir, copyFile } from 'fs/promises'
 
 export default defineConfig({
     entry: ['src/index.ts'],
+    dts: {
+        entry: 'src/index.ts'
+    },
     splitting: false,
     sourcemap: true,
     clean: true,
